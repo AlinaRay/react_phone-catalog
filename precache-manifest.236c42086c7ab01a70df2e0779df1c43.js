beae9bb99ffbe0faefd49cad35c58fc70485fefe
Alina Ray <a.alina.ray@gmail.com>
2019-08-09 17:05:38 +0300

    Updates

---
+++ precache-manifest.236c42086c7ab01a70df2e0779df1c43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204409a45fef7c78f9fe13fd4933a119",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "b4089b1dfcd9af0f5e38",
+    "url": "/react_phone-catalog/static/css/main.988d5b09.chunk.css"
+  },
+  {
+    "revision": "0f1e6f9cc7f85b7e118d",
+    "url": "/react_phone-catalog/static/js/2.6bc507de.chunk.js"
+  },
+  {
+    "revision": "b4089b1dfcd9af0f5e38",
+    "url": "/react_phone-catalog/static/js/main.a60d3369.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
